66556c54222b0db1236bb702b9c9c56034ea82b1
hieuimba <89481020+hieuimba@users.noreply.github.com>
2021-09-21 21:01:18 -0500

    Update Alfred_app.py

--- Alfred_app.py
+++ Alfred_app.py
@@ -3,17 +3,17 @@ from sqlalchemy import create_engine
 import streamlit as st
 import os
 
-#user = os.environ.get('DB_USER')
-#password = os.environ.get('DB_PASSWORD')
-#host = os.environ.get('DB_HOST')
+user = os.environ.get('DB_USER')
+password = os.environ.get('DB_PASSWORD')
+host = os.environ.get('DB_HOST')
 
-user = 'alfred'
-password = 'Alfred:127.0.0.1'
-host = '3.99.99.227'
+#user = 'alfred'
+#password = 'Alfred:127.0.0.1'
+#host = '3.99.99.227'
 
 database = 'prices'
 
 engine = create_engine(f"mysql+mysqlconnector://{user}:{password}@{host}/{database}", echo=False)
 st.write('engine connected')
 example = engine.execute(f"select * from etf_price limit 20")
-st.write(example)
+st.write(example.fetchall())
