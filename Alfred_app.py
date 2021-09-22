8042a352f189bddbf614d5575448254f9431c037
hieuimba <89481020+hieuimba@users.noreply.github.com>
2021-09-22 10:35:12 -0500

    Update Alfred_app.py

--- Alfred_app.py
+++ Alfred_app.py
@@ -6,6 +6,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 import pandas as pd
+import numpy as np
 
 
 ##-------------------------------------------------SETTINGS-----------------------------------------------------------##
