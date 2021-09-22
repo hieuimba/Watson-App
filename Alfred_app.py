7bcd41d39afad6a453962d98a7a817c08191768f
hieuimba <89481020+hieuimba@users.noreply.github.com>
2021-09-21 22:43:50 -0500

    Update Alfred_app.py

--- Alfred_app.py
+++ Alfred_app.py
@@ -23,7 +23,6 @@ password = st.secrets['db_password']
 @st.cache(hash_funcs={sqlalchemy.engine.base.Engine: id})
 def db_connect(db):
     return create_engine(f"mysql+mysqlconnector://{user}:{password}@{host}/{db}")
-
 @st.cache(allow_output_mutation=True, hash_funcs={sqlalchemy.engine.base.Engine: id})
 def run_query(connection, query, index_col = None):
     return pd.read_sql_query(query, connection, index_col)
@@ -31,27 +30,19 @@ def run_query(connection, query, index_col = None):
 positions = db_connect('positions')
 prices = db_connect('prices')
 
-updated = run_query(prices, "SELECT Updated FROM symbol_list LIMIT 1")
-
-option = st.radio('', options = ['Positions','Position Calc','Orders','Sectors','Scanner','Watchlist'])
-st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html = True)
-
-st.caption(f'Updated: {updated.iat[0,0]}')
-st.markdown(f"<h1 style='text-align: center; color: black;'>{option}</h1>", unsafe_allow_html=True)\
-
-
 open_positions = run_query(positions, "SELECT * FROM open_positions", 'symbol')
 open_orders = run_query(positions, "SELECT * FROM open_orders", 'symbol')
 closed_orders = run_query(positions, "SELECT * FROM closed_orders", 'symbol')
 closed_positions = closed_orders.copy()
 
-
-
 ##---------------------------------------------DASHBOARD ELEMENTS-----------------------------------------------------##
 
 ##----------HEADER----------------
+updated = run_query(prices, "SELECT Updated FROM symbol_list LIMIT 1")
 option = st.radio('', options = ['Positions','Position Calc','Orders','Sectors','Scanner','Watchlist'])
 
-st.write("<style>div.row-widget.stRadio > div{flex-direction:row;}</style>", unsafe_allow_html = True)
+st.markdown("<style>div.row-widget.stRadio > div{flex-direction:row;}</style>", unsafe_allow_html = True)
 st.caption(f'Updated: {updated.iat[0,0]}')
 st.markdown(f"<h1 style='text-align: center; color: black;'>{option}</h1>", unsafe_allow_html=True)
+
+
