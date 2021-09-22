fe2399dae6b38c71a0e363f86581381976c7ce5d
hieuimba <89481020+hieuimba@users.noreply.github.com>
2021-09-21 21:32:49 -0500

    Update Alfred_app.py

--- Alfred_app.py
+++ Alfred_app.py
@@ -2,12 +2,51 @@ import mysql.connector
 from sqlalchemy import create_engine
 import streamlit as st
 
-host = st.secrets["db_host"]
-user = st.secrets["db_user"]
-password = st.secrets["db_password"]
-database = 'prices'
 
-engine = create_engine(f"mysql+mysqlconnector://{user}:{password}@{host}/{database}", echo=False)
-st.write('engine connected')
-example = engine.execute(f"select * from etf_price limit 20")
-st.write(example.fetchall())
+
+
+##-------------------------------------------------SETTINGS-----------------------------------------------------------##
+##----------LAYOUT SETUP----------
+st.set_page_config(layout='wide', page_title = 'Alfred 4', page_icon = '📈')
+hide_menu_style = """
+       <style>
+       #MainMenu {visibility: hidden; }
+       footer {visibility: hidden;}
+       </style>
+       """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+risk = st.secrets['risk'] #<--------using static risk
+
+##----------DATABASE SETUP--------
+host = st.secrets['db_host']
+user = st.secrets['db_user']
+password = st.secrets['db_password']
+
+@st.cache(hash_funcs={sqlalchemy.engine.base.Engine: id})
+def db_connect(db):
+    return create_engine(f"mysql://{user}:{password}@{host}/{db}")
+
+#@st.cache(allow_output_mutation=True, hash_funcs={sqlalchemy.engine.base.Engine: id})
+#def get_table(connection, table, index = None):
+#    return pd.read_sql_table(table, con = connection, index_col = index)
+
+@st.cache(allow_output_mutation=True, hash_funcs={sqlalchemy.engine.base.Engine: id})
+def run_query(connection, query):
+    return pd.read_sql_query(query, con = connection)
+
+positions = db_connect('positions')
+prices = db_connect('prices')
+
+updated = run_query(prices, "SELECT Updated FROM symbol_list LIMIT 1")
+
+option = st.radio('', options = ['Positions','Position Calc','Orders','Sectors','Scanner','Watchlist'])
+st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html = True)
+
+st.caption(f'Updated: {updated.iat[0,0]}')
+st.markdown(f"<h1 style='text-align: center; color: black;'>{option}</h1>", unsafe_allow_html=True)\
+
+
+open_positions = run_query(positions, "SELECT * FROM open_positions")
+st.write(open_positions)
+
