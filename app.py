f9f70cf1745c713045fda051f498bb11e6e6934c
hieuimba <89481020+hieuimba@users.noreply.github.com>
2021-09-22 22:15:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import pandas as pd
 import numpy as np
 import matplotlib
 
+from ta import volatility
 
 ##-------------------------------------------------SETTINGS-----------------------------------------------------------##
 ##----------LAYOUT SETUP----------
