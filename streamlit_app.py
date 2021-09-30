f5bb62da1223a8d739b18063f0e2e07537254585
hieuimba <89481020+hieuimba@users.noreply.github.com>
2021-09-30 16:55:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,8 @@ from ta import volatility
 
 ##-------------------------------------------------SETTINGS-----------------------------------------------------------##
 ##----------LAYOUT SETUP----------
-st.set_page_config(layout = 'wide', page_title = 'Alfred 4', page_icon = '🎩')
+icon = open(12608907.jpg)
+st.set_page_config(layout = 'wide', page_title = 'Alfred 4', page_icon = icon)
 st.markdown("<style>#MainMenu {visibility: hidden; } footer {visibility: hidden;}</style>", unsafe_allow_html=True)
 st.markdown("<style>header {visibility: hidden;}</style>", unsafe_allow_html=True)
 
