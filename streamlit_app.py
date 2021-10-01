b4b54345c7def8ec94ffe248ff5551abe2197199
hieuimba <89481020+hieuimba@users.noreply.github.com>
2021-09-30 23:38:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ prices = db_connect('prices')
 ##---------------------------------------------DASHBOARD ELEMENTS-----------------------------------------------------##
 ##----------HEADER----------------
 updated = run_query(positions, "SELECT Updated FROM updated")
-one, two, three = st.columns([0.5,2,20])
+one, two, three = st.columns([1,3,30])
 with one:
     st.image(icon)
 with two:
