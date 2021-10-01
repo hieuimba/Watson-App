8041706c306d6abafe698c720458ec512c4112ad
hieuimba <89481020+hieuimba@users.noreply.github.com>
2021-09-30 23:26:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ from ta import volatility
 
 ##-------------------------------------------------SETTINGS-----------------------------------------------------------##
 ##----------LAYOUT SETUP----------
-icon = Image.open('icon.png')
+icon = Image.open('favicon.ico')
 st.set_page_config(layout = 'wide', page_title = 'Watson 3', page_icon = icon)
 st.markdown("<style>#MainMenu {visibility: hidden; } footer {visibility: hidden;}</style>", unsafe_allow_html=True)
 st.markdown("<style>header {visibility: hidden;}</style>", unsafe_allow_html=True)
