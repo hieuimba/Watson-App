ec279e53c652103acfb824d19515b0ee1a482c32
hieuimba <89481020+hieuimba@users.noreply.github.com>
2021-09-30 23:58:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ with two:
     st.image(icon)
 with three:
     #st.text('Watson 3')
-    st.caption(f'Updated: {updated.iat[0, 0]}')
+    st.text(f'Updated: {updated.iat[0, 0]}')
     
     
 one, two, three = st.columns([1,1,1])
