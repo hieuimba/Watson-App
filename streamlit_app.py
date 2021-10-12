dde2580231a44af56086fa0ade1b15b76bd338ee
hieuimba <hieuimba@gmail.com>
2021-10-11 21:40:13 -0500

    Updated PSC

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ with three:
     
 one, two, three = st.columns([1,3,1])
 with two:
-    option = st.radio('', options = ['Positions', 'Position Calc', 'Orders', 'Sectors', 'Watchlist'])
+    option = st.radio('', options = ['Positions', 'PSC', 'Orders', 'Sectors', 'Watchlist'])
     
 st.markdown("<style>div.row-widget.stRadio > div{flex-direction:row;}</style>", unsafe_allow_html = True)
 st.markdown(f"<h1 style='text-align: center; color: black;'>{option}</h1>", unsafe_allow_html = True)
@@ -239,7 +239,7 @@ if option == 'Sectors':
     st.table(corr_matrix.format(precision = 3))
 
 ##----------CALC SCREEN-----------
-if option == 'Position Calc':
+if option == 'PSC':
     # Get data
     open_positions = run_query(positions, "SELECT * FROM open_positions", 'symbol')
 
