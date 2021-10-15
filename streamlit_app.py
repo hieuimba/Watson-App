fafce769eab9e537f70c5074240e0b6d594412c4
hieuimba <89481020+hieuimba@users.noreply.github.com>
2021-10-14 21:44:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ with three:
     
 one, two, three = st.columns([1,3,1])
 with two:
-    option = st.radio('', options = ['Positions', 'PSC', 'Orders', 'Sectors', 'Watchlist'])
+    option = st.radio('', options = ['Positions', 'PSC', 'Watchlist', 'Orders', 'Sectors'])
     
 st.markdown("<style>div.row-widget.stRadio > div{flex-direction:row;}</style>", unsafe_allow_html = True)
 st.markdown(f"<h1 style='text-align: center; color: black;'>{option}</h1>", unsafe_allow_html = True)
